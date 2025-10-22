8d875db45884bc372d2401792686c29588c5e082
Sean Kovacs <kovacsmillio@gmail.com>
2025-10-22 19:10:49 -0400

    my changes

---
+++ chrome/prefs.js
@@ -0,0 +1,25 @@
+// DO NOT OVERWRITE YOUR PREFS.JS WITH THIS ONE.
+// CLOSE FIREFOX, NAVIGATE TO THE SAME FOLDER
+// YOU PUT chrome/ AND ADD THESE TO THE BOTTOM
+// OF THE EXISTING FILE
+
+user_pref("WaveFox.DragSpace.Tabs", 1);
+user_pref("WaveFox.Icons", 1);
+user_pref("WaveFox.LightTheme.Tabs.Shadows", 4);
+user_pref("WaveFox.Linux.Transparency.Enabled", true);
+user_pref("WaveFox.OneLine", 0);
+user_pref("WaveFox.Tabs.Background.Inactive.Enabled", false);
+user_pref("WaveFox.Tabs.SelectedTabIndicator.Enabled", false);
+user_pref("WaveFox.Tabs.Separators", 0);
+user_pref("WaveFox.Tabs.Shape", 7);
+user_pref("WaveFox.Toolbar.Transparency", 1);
+user_pref("WaveFox.VerticalTabs.WebPage.Floating", 2);
+user_pref("WaveFox.WebPage.Background.Saturation", 1);
+user_pref("WaveFox.WebPage.Floating.Enabled", true);
+user_pref("WaveFox.WebPage.Transparency", 1);
+
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("sidebar.verticalTabs", true);
+user_pref("sidebar.verticalTabs.dragToPinPromo.dismissed", true);
+user_pref("widget.gtk.global-menu.enabled", true);
+user_pref("widget.gtk.global-menu.wayland.enabled", true);
